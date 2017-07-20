fb1689dfc5caa2010bbb6c1429c0f04077745bc0
sjrsimac <sjrsimac@gmail.com>
2017-07-20 01:42:43 -0700

    Reorganizing my bots
    I'm reorganizing my bots so it's easier to maintain and distribute them without having to move my local files around.

--- PollBot.py
+++ PollBot.py
@@ -1,13 +1,12 @@
-import praw
-from credentials import *
-import time
-import sqlite3
+import sys
+sys.path.append('C:/Users/A/Google Drive/Reddit/Reddit Bots/ActiveBots')
+from UniversalFunctions import *
+sys.path.append('')
 import random
-import re
-     
+
 class Record(object):
 
-    def __init__(self, submission):
+    def __init__(self, submission, connection):
         # I'm immediately grabbing the submission and holding it in my object so I don't need to ping reddit.
         self.submission = submission
         self.submission_id = self.submission.fullname
@@ -17,6 +16,8 @@ class Record(object):
         self.author = self.submission.author
         self.karma = self.author.comment_karma + self.author.link_karma
         self.subreddit = self.submission.subreddit
+        self.connection = connection
+        self.cursor = self.connection.cursor()
         
     def CompileOptions(self): # We organize our options
         # print('CompileOptions', self.submission_id)
@@ -30,14 +31,14 @@ class Record(object):
         
     def RecordRequestedPoll(self):
         # print('RecordRequestedPoll', self.submission_id)
-        CheckingRecord = cursor.execute('SELECT ROWID FROM Polls WHERE submission_id=?',(self.submission_id,))
+        CheckingRecord = self.cursor.execute('SELECT ROWID FROM Polls WHERE submission_id=?',(self.submission_id,))
         CheckedRecord = CheckingRecord.fetchall()
         if len(CheckedRecord) == 0 and ('!pollbot' in self.selftext):
             self.CompileOptions()
             if len(self.SampleOptions) > 1: # Confirm we actually have options.
                 # Note the find in SQL.
-                cursor.execute('INSERT OR IGNORE INTO Polls (submission_id, created_utc, recorded_utc) VALUES (?,?,?)', (self.submission_id, self.created_utc, self.recorded_utc))
-                connection.commit()
+                self.cursor.execute('INSERT OR IGNORE INTO Polls (submission_id, created_utc, recorded_utc) VALUES (?,?,?)', (self.submission_id, self.created_utc, self.recorded_utc))
+                self.connection.commit()
                 # And tell everyone how to react to our bot.
                 submission.reply("Hello, I am a pollbot. I was summoned by the author, or original poster (OP), of this post. I am here to help OP quickly check the pulse of reddit. Here's how it works:\n\nOP summoned me with the phrase *!PollBot*, and then followed that key phrase with the options for this poll. For this poll, the options are:\n\n" + self.optionsForExplanation + "\nTo demonstrate your support for a particular position, include one of the phrases, such as *!" + random.sample(self.SampleOptions,1)[0] + "* in your comment, including the exclamation point.\n\nAfter 24 hours, I will return to post a summary of the results.\n\nPlease note that I only count your vote if you have at least 100 karma, and earned at least 5 karma in this subreddit in the last 60 days; or if you are an approved submitter in this subreddit. You are allowed one vote per submission.\n\n*This bot is maintained by u/sjrsimac.*")
     
@@ -92,42 +93,32 @@ class Record(object):
         Counts = Counts[0:len(Counts)-1]
         if len(Header + "\n" + Separator + "\n" + Counts) <= 10000:
             self.submission.reply(Header + "\n" + Separator + "\n" + Counts)
-        cursor.execute('UPDATE Polls SET counted_utc=? WHERE submission_id=?',(time.time(), self.submission_id))
-        connection.commit()
+        self.cursor.execute('UPDATE Polls SET counted_utc=? WHERE submission_id=?',(time.time(), self.submission_id))
+        self.connection.commit()
 
-def main():
-    start = time.time()
-
-    # Your credentials go in a separate file called 'credentials.py'.
-    reddit = praw.Reddit(client_id = client_id,
-                         client_secret = client_secret,
-                         user_agent = user_agent,
-                         username = username,
-                         password = password)
-                     
-    OurSubreddits = 'pollthecrowdsandbox'
+def PollMain(Botname, OurSubreddits, Database):
+    reddit = StartingTheBot(Botname)
+    
     RequiredEarnedScoreOverLastSixtyDaysToVote = 0
 
     # This is where you begin the connection to the SQLite database and prepare the cursor.
-    connection = sqlite3.connect('C:/Users/A/Google Drive/Reddit/Reddit Bots/PollBot/Polls.db')
+    connection = sqlite3.connect(Database)
     cursor = connection.cursor()
     # cursor.execute('CREATE TABLE Polls (submission_id text PRIMARY KEY, created_utc int, recorded_utc float, counted_utc float)') # This line stays commented unless you're making a new database.
     connection.commit()
     
     for submission in reddit.subreddit(OurSubreddits).new(limit=100):
-        CurrentRecord = Record(submission)
+        CurrentRecord = Record(submission, connection)
         # print(CurrentRecord.submission_id)
         CurrentRecord.RecordRequestedPoll()
 
     CheckReadyForCounting = cursor.execute('SELECT submission_id FROM Polls WHERE counted_utc is NULL and created_utc<=?', (time.time()-60,))
     ReadyForCounting = CheckReadyForCounting.fetchall()
     for submission_id in ReadyForCounting:
-        CurrentRecord = Record(reddit.submission(id=submission_id[0][3:]))
+        CurrentRecord = Record(reddit.submission(id=submission_id[0][3:]), connection)
         CurrentRecord.ConductPoll()
 
     connection.close()
-    
-    print((time.time()-start)/60)
         
 if __name__ == '__main__':
-    main()
+    PollMain('pollthecrowd','pollthecrowdsandbox','C:/Users/A/Google Drive/Reddit/Reddit Bots/PollBot/Polls.db')
